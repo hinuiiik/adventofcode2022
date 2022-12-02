360ccec3f223136b768114228bf76a6db7f1386d
hinuiiik <vknc@protonmail.com>
2022-12-01 22:52:18 -0500

    second part of day 1

--- day1/C++/main.cpp
+++ day1/C++/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     fstream input;
-    input.open("../../input");
+    input.open("../input");
     if(!input.is_open()) {
         cout << "nope" << endl;
         return 0;
@@ -13,6 +13,8 @@ int main() {
 
     int largest = 0;
     int current = 0;
+    int second = 0;
+    int third = 0;
     string line;
 
     while(getline(input,line)) {
@@ -22,11 +24,22 @@ int main() {
         else {
             current = 0;
         }
+        // imagine good code...
+        if (current>second && current<largest) {
+            third = second;
+            second = current;
+        }
+        if (current>third && current<second) {
+            third = current;
+        }
         if (current>largest) {
+            third = second;
+            second = largest;
             largest = current;
         }
     }
-    cout << "Largest: " << largest << endl;
+    cout << "Largest: " << largest << endl << "2nd Place: " << second << endl << "3rd Place: " << third << endl;
+    cout << "Combined: " << largest + second + third << endl;
     input.close();
     return 0;
 }
